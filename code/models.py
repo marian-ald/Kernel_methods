7ef082cc438304ce8dc44057ce0d2a809c2b06a8
marian-ald <marian.aldescu11@gmail.com>
2021-01-09 18:28:10 +0100

    Added empty files and whiteboard

--- code/models.py
+++ code/models.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class Models(object):
@@ -6,6 +7,24 @@ class Models(object):
     """
     def __init__(self):
         pass
+
+    def compute_K_matrix(self, train_data):
+
+        return K
+
+
+    def solve_linear_system(self, K, n, lam, y):
+        
+
+        # alpha = np.linalg.solve(A, b)
+
+        return alpha
+
+
+    def predict_labels(self, alpha, K_matrix, test_data):
+
+        return labels
+
 
 
     def KRR(self, x_tr, x_te, y_tr, y_te):
